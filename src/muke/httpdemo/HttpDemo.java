d86de1669cda31793ff6f9040d988c88ef8df796
578380312@qq.com <578380312@qq.com>
2019-03-14 18:19:43 +0800

    http库

---
+++ src/muke/httpdemo/HttpDemo.java
@@ -0,0 +1,10 @@
+package muke.httpdemo;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class HttpDemo {
+    final static CloseableHttpClient client = HttpClients.createDefault();
+    //常用方法1
+
+}
